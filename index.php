24c86e1002e901f3e4c75bf3ab5f1211e76f1b37
Dwiki Alfian <dwikialfian.personal@gmail.com>
2018-11-17 11:26:42 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+// index.php?module=users
+// namanya query string
+$module = empty($_GET['module']) ? 'home' : $_GET['module'];
+
+// routing
+switch($modul) {
+    case "users":
+    include 'modules/users/index.php';
+    break;
+    case "transaksi":
+    include 'modules/transaksi/index.php';
+    break;
+}
+?>
+
